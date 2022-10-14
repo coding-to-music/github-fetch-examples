6ac69e48f1ce8b5e86e4a1cfce37c755b762d4df
coding-to-music <connors.tom@gmail.com>
2022-10-14 20:39:38 +0000

    Build

--- index.js
+++ index.js
@@ -1,78 +1,15 @@
-const mongoose = require("mongoose");
+const fetch = require("node-fetch");
 
-require("dotenv").config();
+module.exports = async function () {
+  console.log("Fetching new github stargazers count…");
 
-const { getClones } = require("./controllers/clones");
-const { getViews } = require("./controllers/views");
-const { getStars } = require("./controllers/stars");
-const { getForks } = require("./controllers/forks");
-
-const uri = process.env.MONGODB_URI;
-
-const connectDB = async () => {
-  try {
-    console.log("Connecting to MongoDB...");
-    await mongoose.connect(uri, {});
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB: ", error.message);
-  }
+  // GitHub API: https://developer.github.com/v3/repos/#get
+  return fetch("https://api.github.com/repos/11ty/eleventy")
+    .then((res) => res.json()) // node-fetch option to transform to json
+    .then((json) => {
+      // prune the data to return only what we want
+      return {
+        stargazers: json.stargazers_count,
+      };
+    });
 };
-
-const jobs = [getClones, getViews, getStars, getForks];
-const repos = [
-  { owner: "coding-to-music", repo: "nestjs-prisma-multi-database" },
-  {
-    owner: "coding-to-music",
-    repo: "grafana-nginx-prometheus-postgres-node-exporter-jmx-cadvisor",
-  },
-  {
-    owner: "coding-to-music",
-    repo: "tempo-otel-open-telemetry-loki-prometheus-grafana",
-  },
-  {
-    owner: "coding-to-music",
-    repo: "docker-flask-mongodb-k8-grafana-mqtt-fastapi",
-  },
-  { owner: "coding-to-music", repo: "k6-influxdb-grafana-docker-load-testing" },
-  { owner: "coding-to-music", repo: "postgres-postgrest-cloudflare-docker" },
-  { owner: "coding-to-music", repo: "facebook-firebase-next-chat" },
-  { owner: "coding-to-music", repo: "chalice-workshop" },
-  { owner: "coding-to-music", repo: "mongoose-mongodb-upload-images" },
-  { owner: "coding-to-music", repo: "nuxt-supabase-full-multi-user-blog" },
-  { owner: "coding-to-music", repo: "battlecode2021" },
-  { owner: "coding-to-music", repo: "coding-to-music" },
-  { owner: "coding-to-music", repo: "sass-mern-image-uploader" },
-  { owner: "coding-to-music", repo: "coding-to-music.github.io" },
-];
-
-const runCronJob = async () => {
-  try {
-    await connectDB();
-    console.log("------- started -------");
-    await Promise.all(
-      jobs.map((job) => repos.map(({ owner, repo }) => job(owner, repo)))
-    );
-  } catch (error) {
-    console.error(error.message);
-  }
-};
-
-async function main() {
-  // 30s
-  const ms = 30 * 1000;
-
-  try {
-    await runCronJob();
-    // stop cron job
-    setTimeout(async () => {
-      console.log("------- stopped -------");
-      await mongoose.disconnect();
-      console.log("Disconnected from MongoDB");
-    }, ms);
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-main();
