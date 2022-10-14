14dc757d5f46a534d9e072a10a293e3e9a1cc636
coding-to-music <connors.tom@gmail.com>
2022-10-14 21:36:15 +0000

    Build

--- index.cjs
+++ index.cjs
@@ -17,12 +17,39 @@ const stars = async () => {
     });
 };
 
+const forks = async () => {
+  console.log("Fetching new github forks count…");
+
+  // GitHub API: https://developer.github.com/v3/repos/#get
+  return fetch("https://api.github.com/repos/11ty/eleventy")
+    .then((res) => res.json()) // node-fetch option to transform to json
+    .then((json) => {
+      // prune the data to return only what we want
+      console.log("Forks: ", json.forks_count);
+      return {
+        forks: json.forks_count,
+      };
+    });
+};
+
 async function main() {
   // 15s
-  const ms = 15 * 1000;
+  const ms = 1 * 1000;
 
   try {
     await stars();
+    // stop cron job
+    setTimeout(async () => {
+      console.log("------- stopped -------");
+      // await mongoose.disconnect();
+      // console.log("Disconnected from MongoDB");
+    }, ms);
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  try {
+    await forks();
     // stop cron job
     setTimeout(async () => {
       console.log("------- stopped -------");
